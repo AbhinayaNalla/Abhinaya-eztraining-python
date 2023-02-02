9a1e7dfe1f618a9552ee3979992a8f31f88dd539
AbhinayaNalla <108124797+AbhinayaNalla@users.noreply.github.com>
2023-02-02 22:05:20 +0530

    Add files via upload

---
+++ day1-programs.py
@@ -0,0 +1,62 @@
+'''a,b=input("Enter").split(",")
+print(a)
+print(type(a))
+print(b)
+print(a+b) *** MULTIPLE INPUTS***
+
+pnum=45
+nnum=-24.6
+multiply=(pnum*nnum)
+print(multiply) ***MULTIPLY***
+
+length=int(input("Enter length:"))
+breadth=int(input("Enter breadth:"))
+area=(length*breadth)
+print("area of rectangle:")
+print(area)*** AREA OF RECTANGLE***
+
+length=int(input("Enter length:"))
+breadth=int(input("Enter breadth:"))
+perimeter=2*(length+breadth)
+print("perimeter of rectangle:")
+print(perimeter) *** PERIMETER OF RECTANGLE***
+
+n=int(input("Enter integerno1:"))
+n1=int(input("Enter integerno2:"))
+n2=int(input("Enter integerno3:"))
+print("\n")
+n3=float(input("Enter floatno1:"))
+n4=float(input("Enter floatno2:"))
+n5=float(input("Enter floatno3:"))
+print("\n")
+n6=input("Enter string1:")
+n7=input("Enter string2:")
+print("\n")
+n8=complex(input("Enter complex1:"))
+print("OUTPUT")
+print("\n")
+print("First integer:",n)
+print("second integer:",n1)
+print("Third integer:",n2)
+print("\n")
+print("First float",n3)
+print("second float",n4)
+print("Third float",n5)
+print("\n")
+print("first string",n6)
+print("second string",n7)
+print("\n")
+print("first complex",n8) *** PRINTING MULTIPLE DATA TYPES VALUES***
+
+sc=int(input("Enter the amount of sugar candy kumar bought:"))
+print("kumar gave sam half of the sugar candy")
+sam=(sc/2)
+print(sam)
+print("sam returned to kumar half in what he got")
+kumar=(sam/2)
+print(kumar)
+final=(sam+kumar)
+print("The amount of candy kumar is having:")
+print(final) *** SUGAR CANDY*** '''
+
+
---
+++ day2-programs.py
@@ -0,0 +1,65 @@
+"""
+a,b=int(input("enter the number <=15:")),int(input("enter the number <=15:"))
+print(a|b)
+print(a&b)
+print(a^b)
+print(a>>2,a<<2)
+print(b>>2,b<<2)
+
+n=int(input("size"))
+a=list(map(int,input("numbers").strip().split()))[:n]
+print(a)
+
+find product of 10 int num
+
+a=list(map(int,input("numbers").strip().split()))
+for var in a:
+  print(var*a)
+
+print("its","a","good","day",end='')
+print("all","is","good",sep="**",end='')
+print("joy")
+
+print("*","*","*","*","*",end='\n')
+print("","*","","","","","*",end='\n')
+print("","","*","","","*",end='\n')
+print("","","","","*","",end='\n')
+
+if,elif,elif ladder,nested if
+
+
+n= int(input("enter the num"))
+if (n==500):
+    print("the no is found")
+else:
+    print("the no is not found")
+
+n=int(input(" enter the num :"))
+n1=int(input("divisor:"))
+if(n%n1==0):
+    print("it is divisible by n1")
+else:
+    print("it is nopt divisible")
+
+print("Before swap")
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+temp=a
+a=b
+b=temp
+print("After swap")
+print(a)
+print(b)
+
+print("before swap:")
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+b=b-a
+a=a+b
+print("After swap:")
+print(a)
+print(b)
+
+
+
+          
